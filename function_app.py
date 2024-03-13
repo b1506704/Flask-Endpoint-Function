3f2bc40c6947c461edca3315f41f9fd94459302b
Anh Le <Anh.Le@webstercare.com.au>
2024-03-13 22:58:32 +0700

    .

--- function_app.py
+++ function_app.py
@@ -24,7 +24,7 @@ def prompt_handler(req: func.HttpRequest) -> func.HttpResponse:
         prompt = req_body.get('prompt')
 
     if prompt:
-        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
